176126edc5f81a2a8c7f90c28f12800f2ef354d6
Adam <adam.nemeth.0315@gmail.com>
2021-02-20 10:54:58 +0100

    git push try

---
+++ src/app/page/events-list/events-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Event } from 'src/app/model/event';
+import { EventService } from 'src/app/service/event.service';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-events-list',
+  templateUrl: './events-list.component.html',
+  styleUrls: ['./events-list.component.scss']
+})
+export class EventsListComponent implements OnInit {
+
+  eventList: BehaviorSubject<Event[]> = this.eventService.list$;
+  testEvent: Observable<Event> = this.eventService.get(1);
+
+  constructor(
+    private eventService: EventService,
+  ) { }
+
+  ngOnInit(): void { }
+
+  onDelete(event: Event): void {
+    this.eventService.remove(event).subscribe(
+      () => console.log('deleted')
+    );
+  }
+}
